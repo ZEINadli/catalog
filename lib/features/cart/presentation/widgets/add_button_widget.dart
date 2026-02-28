42d219559a44ffd4fbab6b5dac267419ed41337c
ZEINadli <1123150028@global.ac.id>
2026-02-28 19:34:30 +0700

    membuat UI AddButtonWidget

--- lib/features/cart/presentation/widgets/add_button_widget.dart
+++ lib/features/cart/presentation/widgets/add_button_widget.dart
@@ -1,10 +1,21 @@
+import 'package:catalog/features/cart/domain/entities/product.dart';
+import 'package:catalog/features/cart/presentation/providers/cart_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class AddButtonWidget extends StatelessWidget {
-  const AddButtonWidget({super.key});
+  final Product product;
+  const AddButtonWidget({required this.product, super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final isItemInCart = context.select<CartProvider, bool>(
+      (provider) => provider.isItemInCart(product.id),
+    );
+
+    return TextButton(
+      onPressed: isItemInCart ? null : () => context.read<CartProvider>().addItem(product),
+      child: isItemInCart ? Icon(Icons.check_circle, color: Colors.green) : Text('TAMBAH'),
+    );
   }
 }
