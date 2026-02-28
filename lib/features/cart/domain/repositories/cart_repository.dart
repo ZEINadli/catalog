f690762862d94f1a725faa39cf5f714b84fe80c2
ZEINadli <1123150028@global.ac.id>
2026-02-28 17:06:18 +0700

    membuat kontrak CartRepository

--- lib/features/cart/domain/repositories/cart_repository.dart
+++ lib/features/cart/domain/repositories/cart_repository.dart
@@ -1,3 +1,8 @@
+import 'package:catalog/features/cart/domain/entities/product.dart';
+
 abstract class CartRepository {
-  
+  List<Product> getCartItems();
+  void addItem(Product product);
+  void removeAllItems();
+  bool isItemInCart(String productId);
 }
