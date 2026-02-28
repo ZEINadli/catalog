57c9a1ad6daece6dddb9b4e85872a9d193cbc954
ZEINadli <1123150028@global.ac.id>
2026-02-28 17:11:22 +0700

    membuat List untuk Product

--- lib/features/cart/data/repositories/cart_repository_impl.dart
+++ lib/features/cart/data/repositories/cart_repository_impl.dart
@@ -1,5 +1,6 @@
+import 'package:catalog/features/cart/domain/entities/product.dart';
 import 'package:catalog/features/cart/domain/repositories/cart_repository.dart';
 
 class CartRepositoryImpl implements CartRepository {
-  
+  final List<Product> _items = [];
 }
