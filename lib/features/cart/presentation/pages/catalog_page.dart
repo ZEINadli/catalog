eb8aca24f3d720ddd4dd12b0818a7446dfe62bb2
ZEINadli <1123150028@global.ac.id>
2026-02-28 19:31:04 +0700

    membuat daftar menu

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -1,7 +1,14 @@
+import 'package:catalog/features/cart/domain/entities/product.dart';
 import 'package:flutter/material.dart';
 
 class CatalogPage extends StatelessWidget {
   const CatalogPage({super.key});
+
+  static final List<Product> _products = [
+    Product(id: '1', name: 'Siomay', price: '5000'),
+    Product(id: '2', name: 'Americano', price: '15000'),
+    Product(id: '3', name: 'Katsu', price: '10000'),
+  ];
 
   @override
   Widget build(BuildContext context) {
