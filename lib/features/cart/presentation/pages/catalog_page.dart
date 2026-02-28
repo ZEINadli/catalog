26a26aa431a88b614e02b3318e4aecd91f555074
ZEINadli <1123150028@global.ac.id>
2026-02-28 20:16:37 +0700

    membuat tampilan catalog

--- lib/features/cart/presentation/pages/catalog_page.dart
+++ lib/features/cart/presentation/pages/catalog_page.dart
@@ -1,4 +1,6 @@
+import 'package:catalog/core/routes/app_router.dart';
 import 'package:catalog/features/cart/domain/entities/product.dart';
+import 'package:catalog/features/cart/presentation/widgets/add_button_widget.dart';
 import 'package:flutter/material.dart';
 
 class CatalogPage extends StatelessWidget {
@@ -11,7 +13,56 @@ class CatalogPage extends StatelessWidget {
   ];
 
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text("Menu"),
+      actions: [
+        IconButton(
+          onPressed: () =>
+              Navigator.pushNamed(context, AppRouter.cart),
+          icon: const Icon(Icons.shopping_cart, color: Colors.yellow,),
+        )
+      ],
+    ),
+    body: ListView.separated(
+      padding: const EdgeInsets.all(16),
+      itemCount: _products.length,
+      separatorBuilder: (_, __) => const Divider(height: 24),
+      itemBuilder: (context, index) {
+        final product = _products[index];
+
+        return Row(
+          children: [
+            const Icon(Icons.food_bank, size: 28, color: Colors.yellow,),
+            const SizedBox(width: 16),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name,
+                    style: const TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    "Rp ${product.price}",
+                    style: const TextStyle(
+                      fontSize: 14,
+                      color: Colors.grey,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            AddButtonWidget(product: product),
+          ],
+        );
+      },
+    ),
+  );
+}
 }
