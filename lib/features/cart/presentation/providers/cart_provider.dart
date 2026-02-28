ca85f483e0579804625581295ac050c1fea26c80
ZEINadli <1123150028@global.ac.id>
2026-02-28 19:25:21 +0700

    membuat constructor dan get cart items class CartProvider

--- lib/features/cart/presentation/providers/cart_provider.dart
+++ lib/features/cart/presentation/providers/cart_provider.dart
@@ -1,5 +1,12 @@
+import 'package:catalog/features/cart/domain/entities/product.dart';
+import 'package:catalog/features/cart/domain/repositories/cart_repository.dart';
 import 'package:flutter/material.dart';
 
 class CartProvider extends ChangeNotifier {
-  
+  final CartRepository _repository;
+
+  CartProvider({required CartRepository repository}) : _repository = repository;
+
+  List<Product> get items => _repository.getCartItems();
+
 }
