ebb342e827fd25fd95fefade8e0b33f9377df764
ZEINadli <1123150028@global.ac.id>
2026-02-28 20:15:58 +0700

    membuat injection

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,5 +1,14 @@
+import 'package:catalog/features/cart/data/repositories/cart_repository_impl.dart';
+import 'package:catalog/features/cart/presentation/providers/cart_provider.dart';
+import 'package:catalog/main.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 Widget buildApp(){
-  
+  final cartRepository = CartRepositoryImpl();
+
+  return ChangeNotifierProvider(
+    create: (context) => CartProvider(repository: cartRepository),
+    child: MyApp(),
+  );
 }
